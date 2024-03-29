ea4af830100ee07dcbcaa55199ec974ac03dd4be
jajang_mahrul <jajangmahrul22@gmail.com>
2024-03-29 10:22:17 +0700

    Add product index view and update ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index(): View
+    {
+        $products = Product::latest()->paginate(10);
+
+        return view('products.index', compact('products'));
+    }
 }
