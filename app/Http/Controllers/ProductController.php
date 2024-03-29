587c7bc08b3f540f82669704c61005c2ca71669b
jajang_mahrul <jajangmahrul22@gmail.com>
2024-03-29 14:10:36 +0700

    Add edit and update methods to ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,7 @@ use Illuminate\View\View;
 use App\Models\Product;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -52,5 +53,50 @@ class ProductController extends Controller
         $product = Product::findOrFail($id);
 
         return view('products.show', compact('product'));
+    }
+
+    public function edit(string $id): View
+    {
+        $product = Product::findOrFail($id);
+
+        return view('products.edit', compact('product'));
+    }
+
+    public function update(Request $request, $id): RedirectResponse
+    {
+        $request->validate([
+            'image' => 'image|mimes:jpeg,png,jpg|max:2048',
+            'title' => 'required|min:5',
+            'description' => 'required|min:10',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric'
+        ]);
+
+        $product = Product::findOrFail($id);
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $image->storeAs('public/products', $image->hashName());
+
+            Storage::delete('public/products/' . $product->image);
+            $product->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'description' => $request->description,
+                'price' => $request->price,
+                'stock' => $request->stock,
+            ]);
+        } else {
+            $product->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'price' => $request->price,
+                'stock' => $request->stock,
+            ]);
+        }
+
+
+        return redirect()->route('products.index')
+            ->with('success', 'Data Berhasil Diupdate!');
     }
 }
